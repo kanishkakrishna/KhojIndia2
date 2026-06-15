9f3862093ee1ae4cace1eea3f013c23b058b6888
kanishka <kanishkakrishna694@gmail.com>
2026-06-16 00:39:51 +0530

    force fix homepage import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Places from "./pages/Places";
 import Search from "./pages/Search";
 import Leaderboard from "./pages/Leaderboard";
 import Navbar from "./components/Navbar";
-import Welcome from "./pages/Homepage";
+import Welcome from "./pages/HomeMain";
 import PlaceDetails from "./pages/PlaceDetails";
 import AiTripPlanner from "./pages/AiTripPlanner";
 
